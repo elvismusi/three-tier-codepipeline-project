b66134394925df6a4cdb93d67445744c47ace6c3
elvismusi <139410844+elvismusi@users.noreply.github.com>
2024-02-18 13:53:42 -0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   project_tags = {    
-    contact = "info@romiandsons.com"
+    contact = "musielvisagrey@gmail.com"
     application = "payments"
     project = "demo-project"
     environment = "${terraform.workspace}"

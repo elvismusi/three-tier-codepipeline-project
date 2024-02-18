477bd9a07f263ec2f018f5fee94e078751c331e8
elvismusi <139410844+elvismusi@users.noreply.github.com>
2024-02-18 14:41:18 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "codepipe-bucket"
+    bucket = "codepipe-bucket1"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
     workspace_key_prefix  = "env"
